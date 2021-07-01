8de62407cd51934ba234167603fb2b9524e65d36
Crusologue <46818212+Crusolog@users.noreply.github.com>
2021-07-01 16:02:27 +0300

    Add files via upload
    first uploading

---
+++ target/debug/deps/newpoject.d
@@ -0,0 +1,5 @@
+C:\Users\jeanc\Desktop\newpoject\target\debug\deps\newpoject.exe: src\main.rs
+
+C:\Users\jeanc\Desktop\newpoject\target\debug\deps\newpoject.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/newpoject.d
@@ -0,0 +1,1 @@
+C:\Users\jeanc\Desktop\newpoject\target\debug\newpoject.exe: C:\Users\jeanc\Desktop\newpoject\src\main.rs
